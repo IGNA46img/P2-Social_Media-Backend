4ff0fbee6c2e25ea347435f0b44a66151502bcc5
IGNA46img <img.872122@gmail.com>
2025-09-22 01:14:54 +0200

    User model, Register endpoint and dbConnection

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+const dbConnection = require("./config/db");
+dbConnection();
 
+app.use(express.json());
 app.get("/", (req, res) => res.send("Welcome!"));
 
 const routes = ["auth", "comments", "posts", "users"];
-
 routes.forEach((route) => app.use("/" + route, require("./routes/" + route)));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
